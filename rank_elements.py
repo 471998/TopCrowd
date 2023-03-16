0d4a25d784b29723a06d40b443f36bee7e2cc78c
mallika <mallikashaik566@gmail.com>
2023-03-16 16:39:47 +0530

    added another file

---
+++ rank_elements.py
@@ -0,0 +1,8 @@
+Input=[10, 8, 15, 12, 6, 20, 1]
+##Output=[4, 3, 6, 5, 2, 7, 1 ]
+opt=[]
+new_inp=sorted(Input)
+#print(new_inp)
+for i in Input:
+    if i in new_inp:
+        print((new_inp.index(i)+1))
