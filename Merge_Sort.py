50af8b929680fb94c8717ac89c8ef3c99e0df69b
mallika <mallikashaik566@gmail.com>
2023-04-04 16:10:02 +0530

    added one file

---
+++ Merge_Sort.py
@@ -0,0 +1,36 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Divide the array into two halves
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort the left and right halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merge the sorted halves
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+arr = [5, 3, 8, 6, 2, 7, 1, 4]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
