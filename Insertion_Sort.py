28a57c581a02c7d8bd891534ed0f2f8184d52734
mallika <mallikashaik566@gmail.com>
2023-04-05 18:45:29 +0530

    added one file

---
+++ Insertion_Sort.py
@@ -0,0 +1,15 @@
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage
+arr = [12, 10, 13, 4, 5]
+insertionSort(arr)
+print("Sorted array:", arr)
