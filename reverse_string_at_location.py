1f223b0696e7bff9d171c474f4dc4bf4308c0bf0
mallika <mallikashaik566@gmail.com>
2023-04-10 10:34:30 +0530

    added one file

---
+++ reverse_string_at_location.py
@@ -0,0 +1,19 @@
+##lis=[10, 20, 30, 40, 50, 60, 70, 80]
+##pos_lis=lis[2:5]
+###print(pos_lis)
+##new_lis=lis[0:2]+pos_lis[::-1]+lis[4:len(lis)]
+##print(new_lis)
+
+#op:[10, 20, 50, 40, 30, 50, 60, 70, 80]
+
+##lis=[10, 20, 50, 40, 30, 60, 70, 80]
+##pos_lis=lis[6:8]
+##new_lis=lis[0:6]+pos_lis[::-1]
+##print(new_lis)
+##
+##op:[10, 20, 50, 40, 30, 60, 80, 70]
+
+
+lis=[10, 20, 50, 40, 30, 60, 80, 70]
+new_lis=lis[::-1]
+print(new_lis)
