6468e7ba2f75a3af816c256e687cca58a6035848
mallika <mallikashaik566@gmail.com>
2023-04-13 15:35:42 +0530

    added one file

---
+++ first_last_exchange_string.py
@@ -0,0 +1,9 @@
+string='abcd'
+new_string=string[-1]+string[1:len(string)-1]+string[0]  
+print(new_string)
+
+
+
+string='12345'
+new_string=string[-1]+string[1:len(string)-1]+string[0]  
+print(new_string)
