78263a35825cbed0bf923a35eb69200d7f74d856
mallika <mallikashaik566@gmail.com>
2023-04-24 14:48:58 +0530

    added one file

---
+++ shallowcopy_set.py
@@ -0,0 +1,9 @@
+# initialzing the set
+number_set = {1, 2, 3, 4, 5}
+# assigning the set another variable
+number_set_copy = number_set
+# changing the first value of number_set_copy
+number_set_copy.add(6)
+# printin the both sets
+print(f"Set One: {number_set}")
+print(f"Set Two: {number_set_copy}")
